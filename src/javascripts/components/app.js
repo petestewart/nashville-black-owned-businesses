89d089689f8301e66171c82d3d407407867be03e
Pete Stewart <petestew@gmail.com>
2020-06-07 14:38:25 -0500

    made filter buttons area static

--- src/javascripts/components/app.js
+++ src/javascripts/components/app.js
@@ -1,8 +1,20 @@
 import biz from '../helpers/data/bizData';
 import utils from '../helpers/utils';
 
-const displayAll = () => {
+const filterbyNeighborhood = () => {
+  let filteredList = [];
+  const filters = ['East Nashville', 'Antioch', 'Germantown'];
   const list = biz.getBusinesses().slice();
+  for (let i = 0; i < filters.length; i += 1) {
+    const results = list.filter((shop) => shop.neighborhood.includes(filters[i]));
+    // filteredList.push(results);
+    filteredList = filteredList.concat(results);
+  }
+  return filteredList;
+};
+
+const displayResults = (array) => {
+  const list = array.slice();
   list.sort((a, b) => {
     let nameA = a.name.toUpperCase(); // ignore upper and lowercase
     let nameB = b.name.toUpperCase(); // ignore upper and lowercase
@@ -21,7 +33,6 @@ const displayAll = () => {
     // names must be equal
     return 0;
   });
-
   let domString = '';
   for (let i = 0; i < list.length; i += 1) {
     const business = list[i];
@@ -38,4 +49,4 @@ const displayAll = () => {
   utils.printToDom('#results', domString);
 };
 
-export default { displayAll };
+export default { displayResults, filterbyNeighborhood };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,9 +4,13 @@ import '@fortawesome/fontawesome-free';
 import app from './components/app';
 import header from './components/header';
 import filters from './components/filters';
+import data from './helpers/data/bizData';
 
 const init = () => {
-  app.displayAll();
+  const all = data.getBusinesses();
+  // const east = app.filterbyNeighborhood();
+  // console.error(east);
+  app.displayResults(all);
   header.createHeader();
   filters.createFilterButtons();
   // window.onscroll = () => { header.makeStick(); };
