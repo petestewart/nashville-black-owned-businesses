bd782112f35dd034fb918f1b5630a068e6790cc1
Pete Stewart <petestew@gmail.com>
2020-06-07 19:10:10 -0500

    neighborhood filters function and buttons display work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,16 +3,13 @@ import 'bootstrap';
 import '@fortawesome/fontawesome-free';
 import app from './components/app';
 import header from './components/header';
-import filters from './components/filters';
-import data from './helpers/data/bizData';
 
 const init = () => {
-  const all = data.getBusinesses();
-  // const east = app.filterbyNeighborhood();
-  // console.error(east);
-  app.displayResults(all);
+  // const all = data.getBusinesses();
+  // app.displayResults(all);
   header.createHeader();
-  filters.createFilterButtons();
+  app.createFilterButtons();
+  app.displayAll();
   // window.onscroll = () => { header.makeStick(); };
   // header.makeStick();
 };
