70501b6e34ad964290aa6c2ab7f3fb2bbba6f079
Pete Stewart <petestew@gmail.com>
2020-06-12 00:11:45 -0500

    filters mostly working need to fix buttons

--- src/javascripts/helpers/utils.js
+++ src/javascripts/helpers/utils.js
@@ -6,4 +6,17 @@ const sortList = (array, key) => {
   array.sort((a, b) => (a[key]) - (b[key]));
 };
 
-export default { printToDom, sortList };
+
+const toggleBoxOn = (id) => {
+  document.getElementById(id).classList.remove('fa-square');
+  document.getElementById(id).classList.add('fa-check-square');
+};
+
+const toggleBoxOff = (id) => {
+  document.getElementById(id).classList.remove('fa-check-square');
+  document.getElementById(id).classList.add('fa-square');
+};
+
+export default {
+  printToDom, sortList, toggleBoxOn, toggleBoxOff,
+};
